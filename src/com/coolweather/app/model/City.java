70b9abdae4d7ef8b2c3111a8ecf46ae79ff85b0a
alkgla <1541988423@qq.com>
2016-12-03 13:48:02 +0800

    新增数据库帮助类，以及对应的实体类

---
+++ src/com/coolweather/app/model/City.java
@@ -0,0 +1,34 @@
+package com.coolweather.app.model;
+
+/**
+ * Created by Administrator on 2016/12/3.
+ */
+public class City {
+    private int id;
+    private String cityName;
+    private String cityCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
---
+++ src/com/coolweather/app/model/Country.java
@@ -0,0 +1,34 @@
+package com.coolweather.app.model;
+
+/**
+ * Created by Administrator on 2016/12/3.
+ */
+public class Country {
+    private int id;
+    private String countryName;
+    private String countryCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+}
